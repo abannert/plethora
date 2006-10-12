66fd43cc1d0ac3d368d3a2f6797da53783e023cd
Aaron Bannert <aaron@codemass.com>
2006-10-12 21:56:12 +0000

    include netinet/in.h before arpa/inet.h to avoid warnings on FreeBSD about struct in_addr being incomplete

--- balancer.c
+++ balancer.c
@@ -9,8 +9,8 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <arpa/inet.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 #include <errno.h>
 

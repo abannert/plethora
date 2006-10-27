d61e7b6f06c89e64ca22d7bef16a66f7af45c699
Aaron Bannert <aaron@codemass.com>
2006-10-27 00:46:19 +0000

    add copyright, license and doc comments

--- ring.h
+++ ring.h
@@ -1,8 +1,22 @@
-/**
- * (c) 2006 Codemass, Inc.
- * Author: Aaron Bannert
+/* Copyright 2006 Codemass, Inc.
  *
- * Simple definition of a RING (a double-linked list)
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file ring.h
+ * @brief Simple doubly-linked ring structure inspired by Apache's apr_ring.h
+ * @author Aaron Bannert (aaron@codemass.com)
  */
 
 #ifndef __ring_h
